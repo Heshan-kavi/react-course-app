d8a2a3bf424ee1e79119e4a322f1dadf46b38a67
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-13 12:13:17 +0530

    Added proper links in the main navigation to the other pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import MainNavigation from './components/MainNavigation';
 import AllMeetups from './pages/AllMeetups';
 import Favourites from './pages/Favourites';
 import NewMeetUp from './pages/NewMeetup';
@@ -7,6 +8,7 @@ import NewMeetUp from './pages/NewMeetup';
 function App() {
   return (
     <div>
+      <MainNavigation/>
       <Routes>
         <Route path="/" element={<AllMeetups/>} />
         <Route path="/favourites" element={<Favourites/>} />
--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,4 +1,18 @@
+import { Link } from "react-router-dom";
+import AllMeetups from "../pages/AllMeetups";
+import Favourites from "../pages/Favourites";
+import NewMeetUp from "../pages/NewMeetup";
+
 function MainNavigation (){
+    return (
+        <div>
+            <ul>
+                <li><Link to='/'> All Meetups </Link></li>
+                <li><Link to='/favourites'> My Favourite Meetups </Link></li>
+                <li><Link to='/newmeetup'> New Meetup </Link></li>
+            </ul> 
+        </div>
+    )
 
 }
 
