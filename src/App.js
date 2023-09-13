8f7f768cfa79293ec01259d0d0395c049a20cee5
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-13 12:02:39 +0530

    Added browser router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AllMeetups from './pages/AllMeetups';
+import Favourites from './pages/Favourites';
+import NewMeetUp from './pages/NewMeetup';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetups/>} />
+        <Route path="/favourites" element={<Favourites/>} />
+        <Route path="/newmeetup" element={<NewMeetUp/>} />
+      </Routes>
     </div>
   );
 }
