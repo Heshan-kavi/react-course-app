ee1f68139b8771d68186db34fcafd6d0e56bc73d
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-18 16:44:30 +0530

    Added "save faourites" button

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,14 +1,22 @@
 import { useSelector } from "react-redux";
 import MeetupList from "../components/meetups/MeetupList";
+import classes from './Favourites.module.css';
 
 function Favourites (){
 
   const favouriteList = useSelector(state => state.favourite.favouriteList)
 
+  function confirmFavourites(){
+    console.log(favouriteList);
+  }
+
     return (
         <section>
         <h1>My Favourites meetings</h1>
         <MeetupList meetups={favouriteList}/>
+        <div className={classes.actions}>
+        <button onClick={confirmFavourites}>Save Favourites</button>
+        </div>
       </section>
     )
 }
