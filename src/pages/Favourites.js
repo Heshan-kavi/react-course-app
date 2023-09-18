f9ef55d2bbc783859afacec993c33dfc3d14eacc
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-18 12:06:15 +0530

    Implemented to view the favourite list in the favourite page

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux";
 import MeetupList from "../components/meetups/MeetupList";
 
 function Favourites (){
 
+  const favouriteList = useSelector(state => state.favourite.favouriteList)
+
     return (
         <section>
         <h1>My Favourites meetings</h1>
-        <MeetupList meetups={[{id:12, title:'testing', description:'testing description', address:'testing address', image:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Saint_Basil%27s_Cathedral_and_the_Red_Square.jpg/800px-Saint_Basil%27s_Cathedral_and_the_Red_Square.jpg'}]}/>
+        <MeetupList meetups={favouriteList}/>
       </section>
     )
 }
