729dbd99f8e1c497d911de62ee53acbc66e36fa2
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-18 16:32:22 +0530

    removed unwanted function

--- src/store/reducers/FavouriteSlice.js
+++ src/store/reducers/FavouriteSlice.js
@@ -17,13 +17,9 @@ export const FavouriteSlice = createSlice({
     removeMeetup: (state,action) => {
         state.favouriteList = state.favouriteList.filter(meetup => meetup.id !== action.payload.id);
         state.numOfItemsInTheFavList -= 1
-    },
-    isItemFavouriteItem: (state,action) => {
-      console.log('hello')
-        state.favouriteList.some(meetup => meetup.id === action.payload.id);
     }
   },
 }) 
 
-export const { addNewMeetup, removeMeetup, isItemFavouriteItem} = FavouriteSlice.actions
+export const { addNewMeetup, removeMeetup } = FavouriteSlice.actions
 export default FavouriteSlice.reducer
