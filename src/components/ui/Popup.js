4a77a144fd7552e4cabc8cb81baa550da6406560
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-20 17:38:40 +0530

    updated popup component

--- src/components/ui/Popup.js
+++ src/components/ui/Popup.js
@@ -1,6 +1,28 @@
-function Popup (){
+import classes from './Popup.module.css';
+
+function Popup (props){
+
+    let buttonArea = <></>; 
+
+    if(props.typeOfThePopup === 0){
+        buttonArea =  (<button className={classes.btn__alt} onClick={props.onCancleClick}>{props.buttonText1}</button>)
+    }else if(props.typeOfThePopup === 1){
+        buttonArea = (<>
+            <button className={classes.btn__alt} onClick={props.onCancleClick}>{props.buttonText1}</button>
+            <button className={classes.btn} onClick={props.onConfirmClick}>{props.buttonText2}</button>
+        </>)
+    }
+
     return (
-        <div>this is the popup page</div>
+        <>
+            <div className={classes.backdrop} />
+            <div className={classes.modal}>
+                <h2 className={classes.h2}>{props.title}</h2>
+                <p>{props.content}</p>
+                {buttonArea}
+            </div>
+        </>
+        
     )
 }
 
