68fc5e224bd1c3145c6681ac329fd1d95b1b71d0
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-20 17:39:02 +0530

    updated confimation page with the new popup message

--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import classes from './ConfirmationPage.module.css';
+import Popup from "../components/ui/Popup";
 
 function ConfirmationPage(){
 
     const favouriteList = useSelector((state) => state.favourite.favouriteList);
+    const [isConfirm, setIsConfirm] = useState(false);
+    const [description, setDescription] = useState('');
+    const [title, setTitle] = useState('');
+    
 
-    function seeMoreClickHandler (event, description) {
+    function seeMoreClickHandler (event, meetup) {
         event.preventDefault();
-        alert(description);
+        setDescription(meetup.description);
+        setTitle('Description of ' + meetup.title);
+        setIsConfirm(true);
+    }
+
+    function onCancleClickHandler () {
+        setIsConfirm(false);
+    }
+
+    function onConfirmClickHandler () {
+        setIsConfirm(false);
     }
 
     return (
+        <>
         <section>
+            <div>
             <h1>Confirmation for your favourite list</h1>
             <table className={classes.styled_table}>
                 <thead>
@@ -28,15 +46,18 @@ function ConfirmationPage(){
                         <tr key={val.id}>
                             <td>{val.title}</td>
                             <td>{val.address}</td>
-                            <td width="1000px" className={classes.td_customized} >{val.description.substring(0,150)} ... <button className={classes.button_customized} onClick={(event) => seeMoreClickHandler(event,val.description)}>see more</button></td>
+                            <td width="1000px" className={classes.td_customized} >{val.description.substring(0,150)} ... <button className={classes.button_customized} onClick={(event) => seeMoreClickHandler(event,val)}>see more</button></td>
                             <td><input type="image" src={val.image.toString()} alt="photo" width="300px"/></td>
                         </tr>
                     )
                 })}
                 </tbody>
             </table>
+            {isConfirm && <Popup onCancleClick={onCancleClickHandler} onConfirmClick={onConfirmClickHandler} title={title}  content={description} typeOfThePopup={0}  buttonText1={'ok'} />}
+            </div>
       </section>
-        
+      
+      </>
     )
 }
 
